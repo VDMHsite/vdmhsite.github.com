a4c5e60f31d1480222b473fd8267b51035ccbee9
Megalapa <megalapa@mac.local>
2015-10-06 01:51:02 +0300

    lf

--- js/application.js
+++ js/application.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
 
-	$(this).on('click','this', function(){
-			$(this).fadeOut()
-		});
-
 	$('.heading').blurjs({
-		// source: 'body',
-		radius: 100,
-		overlay: 'rgba(255,255,255,0.4)'
+		source: 'body',
+		radius: 10,
+		overlay: 'rgba(255,255,255,0.4)',
+		draggable: true,
 	});
 });
